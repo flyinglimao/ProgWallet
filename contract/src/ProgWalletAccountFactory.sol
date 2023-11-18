b5cd7bcd112afd924c8476be84a2336252ac9c01
flyinglimao <hi@limaois.me>
2023-11-18 12:01:27 +0300

    fix account factory

--- contract/src/ProgWalletAccountFactory.sol
+++ contract/src/ProgWalletAccountFactory.sol
@@ -4,19 +4,22 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import {ProgWalletAccount} from "./ProgWalletAccount.sol";
 import {IVerifier} from "./interfaces/IVerifier.sol";
+import {IEntryPoint} from "./interfaces/IEntryPoint.sol";
 
 contract ProgWalletAccountFactory {
     ProgWalletAccount impl;
 
-    constructor() {
-        impl = new ProgWalletAccount();
+    constructor(IEntryPoint entryPoint) {
+        impl = new ProgWalletAccount(entryPoint);
     }
 
     function create(
         bytes32 salt,
         IVerifier verifier_
     ) external returns (ProgWalletAccount clone) {
-        clone = Clones.clone(impl, salt);
+        clone = ProgWalletAccount(
+            Clones.cloneDeterministic(address(impl), salt)
+        );
         clone.initialize(verifier_);
     }
 }
